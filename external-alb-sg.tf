0a9ebfb3d19861637659b987315738fb399cb874
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-11 20:21:30 +0100

    Add: Added security group and rules for external alb

---
+++ external-alb-sg.tf
@@ -0,0 +1,34 @@
+# external alb Security Group
+resource "aws_security_group" "external-alb" {
+  name = "external-alb-sg"
+  description = "Public internet access"
+  vpc_id = aws_vpc.main.id
+ 
+  tags = {
+    Name        = "external-alb-sg"
+  }
+}
+ 
+
+# security group rules
+ resource "aws_security_group_rule" "public_in_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.external-alb.id
+}
+resource "aws_security_group_rule" "public_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+ 
+  security_group_id = aws_security_group.external-alb.id
+}
+ 
+ 
+
+ 
