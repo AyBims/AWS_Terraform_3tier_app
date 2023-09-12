627ef8faa74a7329ebf6fd1c50dbf2c045a60683
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-12 17:08:57 +0100

    Add: Added security group and rules for internal alb

---
+++ internal-alb-sg.tf
@@ -0,0 +1,44 @@
+# Internal alb Security Group
+resource "aws_security_group" "internal-alb" {
+  name = "internal-alb-sg"
+  description = "Internal ALB"
+  vpc_id = aws_vpc.main.id
+ 
+  tags = {
+    Name        = "internal-alb-sg"
+  }
+}
+ 
+
+# security group rules
+ resource "aws_security_group_rule" "allow_nginx_port80" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.internal-alb.id
+}
+
+ resource "aws_security_group_rule" "allow_nginx_port80" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.internal-alb.id
+}
+
+resource "aws_security_group_rule" "public_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+ 
+  security_group_id = aws_security_group.internal-alb.id
+}
+ 
+ 
+
+ 
