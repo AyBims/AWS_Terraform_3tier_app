d3a85cdbcd7d3a23fffd3b3b9c069421ca5c7e7a
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-13 21:08:18 +0100

    Add: Nginx Launch Template

--- launch-templates.tf
+++ launch-templates.tf
@@ -43,3 +43,52 @@ resource "aws_launch_template" "bastion-launch-template" {
 
   user_data = filebase64("${path.module}/bin/bastion.sh")
 }
+
+
+
+
+# Nginx Launch Template
+
+resource "aws_launch_template" "nginx-launch-template" {
+  name = "nginx"
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 20
+    }
+  }
+
+
+  image_id = ""
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "t2.micro"
+
+  key_name = "devops"
+
+
+  monitoring {
+    enabled = true
+  }
+
+  placement {
+    availability_zone = "us-east-1a"
+  }
+
+
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+
+    tags = {
+      Name            = "nginx-launch-template"
+    }
+  }
+
+  user_data = filebase64("${path.module}/bin/nginx.sh")
+}
