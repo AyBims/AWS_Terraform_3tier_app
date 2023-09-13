846d4f05d91a2acce8f87efb779472e194e2baf7
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-13 19:53:15 +0100

    Add: Nginx security group and security group rules

---
+++ nginx-sg.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "nginx-sg" {
+  name        = "p15-nginx-sg"
+  vpc_id      = aws_vpc.main.id
+  description = "Nginx SG"
+
+
+  tags = {
+    Name            = "nginx-sg"
+  }
+
+}
+
+
+resource "aws_security_group_rule" "allow_port80_from_ext_alb" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.external-alb-sg.id
+  security_group_id        = aws_security_group.nginx-sg.id
+}
+
+
+resource "aws_security_group_rule" "allow_port443_from_ext_alb" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.external-alb-sg.id
+  security_group_id        = aws_security_group.nginx-sg.id
+}
+
+# Add this later 
+
+resource "aws_security_group_rule" "inbound-bastion-ssh-to-nginx" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion-sg.id
+  security_group_id        = aws_security_group.nginx-sg.id
+}
+
+
+resource "aws_security_group_rule" "allow_all_nginx_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.nginx-sg.id
+}
+
