c1b71b3cd2a2974588d4d37250cbf30a09583057
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-14 19:46:43 +0100

    Add: Tooling Launch Template

--- launch-templates.tf
+++ launch-templates.tf
@@ -92,3 +92,50 @@ resource "aws_launch_template" "nginx-launch-template" {
 
   user_data = filebase64("${path.module}/bin/nginx.sh")
 }
+
+
+
+# Tooling Launch Template
+resource "aws_launch_template" "tooling-launch-template" {
+  name = "tooling"
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 20
+    }
+  }
+
+
+  image_id = ""
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "t2.micro"
+
+  key_name = "devops"
+
+
+  monitoring {
+    enabled = true
+  }
+
+  placement {
+    availability_zone = "us-east-1a"
+  }
+
+
+  vpc_security_group_ids = [aws_security_group.tooling-sg.id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+
+    tags = {
+      Name            = "tooling-launch-template"
+    }
+  }
+
+  user_data = filebase64("${path.module}/bin/tooling.sh")
+}
