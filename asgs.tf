c62a02efaadccd3989ae9d487e67db2828e7da76
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-14 11:43:09 +0100

    Add: Nginx Auto Scaling Group

--- asgs.tf
+++ asgs.tf
@@ -26,3 +26,29 @@ resource "aws_autoscaling_group" "bastion-asg" {
 }
 
 
+# Nginx ASG
+resource "aws_autoscaling_group" "nginx-asg" {
+  name                      = "nginx-asg"
+  max_size                  = 1
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+
+  vpc_zone_identifier = [
+    aws_subnet.PrivateSubnet-1.id,
+    aws_subnet.PrivateSubnet-2.id
+  ]
+
+
+  launch_template {
+    id      = aws_launch_template.nginx-launch-template.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    value               = "nginx"
+    propagate_at_launch = true
+  }
+
+}
