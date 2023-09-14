40bf5e24528ffbcb0c237d9483183f236f7cb3fc
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-14 19:48:18 +0100

    Add: Bastion Userdata

---
+++ bin/bastion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# bastion userdata
+
+sudo yum update -y
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo yum install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
+sudo yum install -y mysql
