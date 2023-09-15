31c4d10c4b6c16468d8c6078b2d9961b09697a0c
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-15 18:12:31 +0100

    Add: Wordpress Launch Template

--- launch-templates.tf
+++ launch-templates.tf
@@ -139,3 +139,49 @@ resource "aws_launch_template" "tooling-launch-template" {
 
   user_data = filebase64("${path.module}/bin/tooling.sh")
 }
+
+
+# Wordpress Launch Template
+resource "aws_launch_template" "wordpress-launch-template" {
+  name = "wordpress"
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 20
+    }
+  }
+
+
+  image_id = ""
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "t2.micro"
+
+  key_name = "devops"
+
+
+  monitoring {
+    enabled = true
+  }
+
+  placement {
+    availability_zone = "us-east-1a"
+  }
+
+
+  vpc_security_group_ids = [aws_security_group.wordpress-sg.id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+
+    tags = {
+      Name            = "wordpress-launch-template"
+    }
+  }
+
+  user_data = filebase64("${path.module}/bin/wordpress.sh")
+}
