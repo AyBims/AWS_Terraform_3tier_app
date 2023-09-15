6aa35499c0df769d21462755c7c85eea1b3a969e
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-15 18:19:20 +0100

    Add: Wordpress Auto Scaling Group

--- asgs.tf
+++ asgs.tf
@@ -52,3 +52,31 @@ resource "aws_autoscaling_group" "nginx-asg" {
   }
 
 }
+
+
+# Wordpress ASG
+resource "aws_autoscaling_group" "wordpress-asg" {
+  name                      = "wordpress-asg"
+  max_size                  = 1
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+
+  vpc_zone_identifier = [
+    aws_subnet.privateSubnet-1.id,
+    aws_subnet.privateSubnet-2.id
+  ]
+
+
+  launch_template {
+    id      = aws_launch_template.wordpress-launch-template.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    value               = "wordpress"
+    propagate_at_launch = true
+  }
+
+}
