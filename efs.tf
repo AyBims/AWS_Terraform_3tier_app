af83c17d235fe0c9ba6122e34f3cf6c72554bae8
Ayomide <adeshinaayomide12@yahoo.com>
2023-09-19 20:06:52 +0100

    Add: EFS mount targets and access points

--- efs.tf
+++ efs.tf
@@ -34,3 +34,64 @@ resource "aws_efs_file_system" "project-efs" {
   }
 
 }
+
+
+# first mount target for the EFS 
+resource "aws_efs_mount_target" "subnet-1" {
+  file_system_id  = aws_efs_file_system.project-efs.id
+  subnet_id       = aws_subnet.privateSubnet-1.id
+  security_groups = [aws_security_group.datalayer-sg.id]
+}
+
+
+# second mount target for the EFS 
+resource "aws_efs_mount_target" "subnet-2" {
+  file_system_id  = aws_efs_file_system.project-efs.id
+  subnet_id       = aws_subnet.privateSubnet-2.id
+  security_groups = [aws_security_group.datalayer-sg.id]
+}
+
+
+# Access point for wordpress
+resource "aws_efs_access_point" "wordpress" {
+  file_system_id = aws_efs_file_system.project-efs.id
+
+  posix_user {
+    gid = 0
+    uid = 0
+  }
+
+  root_directory {
+    path = "/wordpress"
+
+    creation_info {
+      owner_gid   = 0
+      owner_uid   = 0
+      permissions = 0755
+    }
+
+  }
+
+}
+
+
+# Access point for tooling
+resource "aws_efs_access_point" "tooling" {
+  file_system_id = aws_efs_file_system.project-efs.id
+  posix_user {
+    gid = 0
+    uid = 0
+  }
+
+  root_directory {
+
+    path = "/tooling"
+
+    creation_info {
+      owner_gid   = 0
+      owner_uid   = 0
+      permissions = 0755
+    }
+
+  }
+}
